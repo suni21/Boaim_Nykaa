be84f3ac7b3fe647111edd5d042909baa2c32a63
suni21 <83060711+suni21@users.noreply.github.com>
2023-03-26 15:34:30 +0530

    new

--- shopNow/js/app.js
+++ shopNow/js/app.js
@@ -115,13 +115,3 @@ new Glide('.gallery-slide', {
     starAt: 0,
     perView: 8,
 }).mount()
-
-const close = document.querySelector(".close");
-const open = document.querySelector(".ham");
-const menu = document.querySelector(".menu");
-close.addEventListener("click", () => {
-  menu.style.visibility = "hidden";
-});
-open.addEventListener("click", () => {
-  menu.style.visibility = "visible";
-});
